f3f275a45be39fde303973c7722990cb65e755a4
Delyx <daclaxton6@gmail.com>
2023-06-18 14:26:03 -0400

    added .github/workflows folder

---
+++ src/appRoutes.js
@@ -0,0 +1,14 @@
+import {Home} from "./components/Home";
+
+const AppRoutes = [
+{
+    index: true,
+    element: <Home />
+},
+{
+    path: '/Home',
+    element: <Home />
+}
+];
+
+export default AppRoutes;
